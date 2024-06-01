a1b5f23b8732013b1724cea3974d23b5874770a2
nismail1 <nimo_ismail@brown.edu>
2024-06-01 14:13:32 -0400

    Update gitignore

--- src/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Compile the Java files
-javac -d ../bin src/foo_parameterization/core/*.java src/foo_parameterization/tests/TestVolume.java
-
-# Run the TestVolume class
-java -cp ../bin foo_parameterization.tests.TestVolume
